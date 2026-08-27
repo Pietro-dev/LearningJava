ef7354f1d5fa6e363cf7709fba54b7e470da0a9c
Pietro-dev <pietro.pietromartins@gmail.com>
2026-08-27 19:54:19 -0300

    feat: Linked Lists

---
+++ src/LinkedList/Main.java
@@ -0,0 +1,25 @@
+package LinkedList;
+
+import java.util.LinkedList;
+
+public class Main {
+    static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>();
+
+        list.add("Paul");
+        list.add("Leto");
+        list.add("Jessica");
+
+        System.out.println(list);
+
+        list.add("Gurney");
+        list.add(2, "Stilgar");
+        System.out.println(list);
+
+        list.remove(2);
+        System.out.println(list);
+
+
+    }
+
+}
